6b12f459dd184c82795c8f0fc7791870974a1b57
RocknCoder <rockncoder@gmail.com>
2015-03-14 10:36:32 -0700

    ex02

---
+++ examples/app.js
@@ -0,0 +1,29 @@
+(function () {
+    "use strict";
+
+    angular.module("ex2", [])
+        .controller("SomeController", ["$scope", function ($scope) {
+            $scope.title = "Click me to expand";
+            $scope.text = "Hi there folks. I am now visible";
+        }])
+        .directive("expander", function () {
+            return {
+                restrict: "EA",
+                replace: true,
+                transclude: true,
+                scope: {
+                    title: "=expanderTitle"
+                },
+                template: "<div>" +
+                '<div class="title" ng-click="toggle()">{{title}}</div>' +
+                '<div class="body" ng-show="showMe" ng-transclude></div>' +
+                '</div>',
+                link: function (scope, element, attrs) {
+                    scope.showMe = false,
+                        scope.toggle = function () {
+                            scope.showMe = !scope.showMe;
+                        }
+                }
+            };
+        });
+});
