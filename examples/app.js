29151b623e4793e21daae479b468197cba899433
RocknCoder <rockncoder@gmail.com>
2015-03-14 11:26:10 -0700

    ex02 working (c) - with expanderTitle & second-line

--- examples/app.js
+++ examples/app.js
@@ -4,7 +4,7 @@
     angular.module("ex2", [])
         .controller("SomeController", ["$scope", function ($scope) {
             //$scope.title = "Click me to expand";
-            $scope.text = "Hi there folks. I am now visible";
+            //$scope.text = "Hi there folks. I am now visible";
         }])
         .directive("expander", function () {
             return {
@@ -12,13 +12,15 @@
                 replace: true,
                 transclude: true,
                 scope: {
-                    title: "@expanderTitle"
+                    title: "@expanderTitle",
+                    line2: "@secondLine"
                 },
                 template: "<div>" +
                 '<div class="title" ng-click="toggle()">{{title}}</div>' +
-                '<div class="body" ng-show="showMe" ng-transclude></div>' +
+                '<div class="body" ng-show="showMe" ><div ng-transclude></div>{{line2}}</div>' +
                 '</div>',
                 link: function (scope, element, attrs) {
+                    scope.line2 =  scope.line2 || "Troy is a programmer";
                     scope.showMe = false;
                     scope.toggle = function () {
                         scope.showMe = !scope.showMe;
